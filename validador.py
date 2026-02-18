5cb8bda1138c20296a624e4dec8ba21b3a3d48e5
Jessica-CSchool <jls2@cesar.school>
2026-02-18 19:43:10 -0300

    Refactor input validation methods in ValidadorFormatos

--- validador.py
+++ validador.py
@@ -6,16 +6,19 @@ class ValidadorFormatos:
     def _validar_entrada(self, valor: str) -> str:
         if not isinstance(valor, str):
             raise ValueError(self.MSG_ERRO_STRING)
-        return re.sub(r'\D', '', valor) #regex que só aceita numeros
+        return re.sub(r'\D', '', valor) #regex que só aceita numeros, se a mascara tiver que ser validada precisa ser trocado!
 
+    # Formato CPF: 000.000.000-00
     def validar_cpf(self, cpf: str) -> bool:
         numeros = self._validar_entrada(cpf)
         return len(numeros) == 11
 
+    # Formato CNPJ: 00.000.000/0000-00
     def validar_cnpj(self, cnpj: str) -> bool:
         numeros = self._validar_entrada(cnpj)
         return len(numeros) == 14
 
+    # Formato CEP: 00000-000
     def validar_cep(self, cep: str) -> bool:
         numeros = self._validar_entrada(cep)
         return len(numeros) == 8

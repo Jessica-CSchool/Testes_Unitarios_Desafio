92004f57b8515c853f30f281532ca9e276ea0e44
Jessica-CSchool <jls2@cesar.school>
2026-02-19 20:26:08 -0300

    Refactor validation methods to use regex patterns

--- validador.py
+++ validador.py
@@ -1,24 +1,35 @@
 import re
 
 class ValidadorFormatos:
+
+    #Mensagem de erro
     MSG_ERRO_STRING = "A entrada deve ser um texto."
 
-    def _validar_entrada(self, valor: str) -> str:
-        if not isinstance(valor, str):
+    # Regex das máscaras
+    PADRAO_CPF = r"^\d{3}\.\d{3}\.\d{3}-\d{2}$" #Formato CPF: 000.000.000-00
+    PADRAO_CNPJ = r"^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$" #Formato CNPJ: 00.000.000/0000-00
+    PADRAO_CEP = r"^\d{5}-\d{3}$" #Formato CEP: 00000-000
+
+
+    def _verificar_formato(self, valor: str, padrao_regex: str, tamanho_esperado: int) -> bool:
+        if not isinstance(valor, str): #se nao for string
             raise ValueError(self.MSG_ERRO_STRING)
-        return re.sub(r'\D', '', valor) #regex que só aceita numeros, se a mascara tiver que ser validada precisa ser trocado!
 
-    # Formato CPF: 000.000.000-00
+        tem_caractere_especial = "." in valor or "-" in valor or "/" in valor
+
+        if tem_caractere_especial: #se usou mascara
+            return bool(re.match(padrao_regex, valor))
+
+        if valor.isdigit(): #se so usou numeros
+            return len(valor) == tamanho_esperado
+
+        return False   # Se tiver caracteres sem ser a máscara correta, é inválido
+
     def validar_cpf(self, cpf: str) -> bool:
-        numeros = self._validar_entrada(cpf)
-        return len(numeros) == 11
+        return self._verificar_formato(cpf, self.PADRAO_CPF, 11)
 
-    # Formato CNPJ: 00.000.000/0000-00
     def validar_cnpj(self, cnpj: str) -> bool:
-        numeros = self._validar_entrada(cnpj)
-        return len(numeros) == 14
+        return self._verificar_formato(cnpj, self.PADRAO_CNPJ, 14)
 
-    # Formato CEP: 00000-000
     def validar_cep(self, cep: str) -> bool:
-        numeros = self._validar_entrada(cep)
-        return len(numeros) == 8
+        return self._verificar_formato(cep, self.PADRAO_CEP, 8)
